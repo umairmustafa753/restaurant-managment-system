e6387207f7a95020ce95bcca105ce35ccfda887c
Umair Mustafa <umairmustafa753@gmail.com>
2021-01-14 16:40:35 +0500

    add react native horizental list package

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,13 +1,39 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
+import BeautifulHorizontalList from "react-native-beautiful-horizontal-list";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
+const staticData = [
+  {
+    title: "Karahi",
+    value: "8,984",
+    unit: "Rs",
+    primaryColor: "#10CFE4",
+    imageSource: require("../../assets/images/favicon.png")
+  },
+  {
+    title: "Kabab",
+    value: "2.6",
+    unit: "Rs",
+    primaryColor: "#c84cf0",
+    imageSource: require("../../assets/images/favicon.png")
+  },
+  {
+    title: "Tikka",
+    value: "9501",
+    unit: "Rs",
+    primaryColor: "#10E471",
+    imageSource: require("../../assets/images/favicon.png")
+  }
+];
+
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      {/* <Text style={styles.title}>Home</Text> */}
+      <BeautifulHorizontalList data={staticData} />
     </View>
   );
 }
@@ -15,8 +41,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
   },
   title: {
     fontSize: 20,
