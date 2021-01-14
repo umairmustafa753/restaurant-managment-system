459f9cd14398245c7e2f3e114c11061ecc8bea01
Umair Mustafa <umairmustafa753@gmail.com>
2021-01-14 13:02:43 +0500

    add initial files

---
+++ navigation/BottomTabNavigator.tsx
@@ -0,0 +1,78 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import HomeScreen from "../screens/HomeScreen";
+import MenuScreen from "../screens/MenuScreen";
+import { BottomTabParamList, HomeParamList, MenuParamList } from "../types";
+
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreenNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        name="Menu"
+        component={MenuScreenNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          )
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: string; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const HomeStack = createStackNavigator<HomeParamList>();
+
+function HomeScreenNavigator() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{ headerTitle: "Home" }}
+      />
+    </HomeStack.Navigator>
+  );
+}
+
+const MenuStack = createStackNavigator<MenuParamList>();
+
+function MenuScreenNavigator() {
+  return (
+    <MenuStack.Navigator>
+      <MenuStack.Screen
+        name="MenuScreen"
+        component={MenuScreen}
+        options={{ headerTitle: "Menu" }}
+      />
+    </MenuStack.Navigator>
+  );
+}
---
+++ types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Menu: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type MenuParamList = {
+  MenuScreen: undefined;
+};
